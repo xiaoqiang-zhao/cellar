f0d5b8bfbcb81cd32f631b3af066961f1f89803e
zhaoxiaoqiang <longze_xq@163.com>
2015-06-20 12:50:39 +0800

    集成端口占用检查，占用后端口号加一再试，直到找到可用端口。

--- server/lib/start-server.js
+++ server/lib/start-server.js
@@ -6,22 +6,25 @@
 var http = require('http');
 var rout = require('./rout');
 var config = require('./config');
-
+var portModel = require('./port');
 var port = config.defaultPort;
 
-// TODO 端口占用检测
-
-var server = http.createServer(function (request, response) {
+http = http.createServer(function (request, response) {
     rout.routRequest(request, response);
 });
-server.listen(port, function () {
-    // 监听失败(如端口冲突)不会执行此回调
-    console.log('本地服务器 http://localhost:' + port + ' 已经启动');
-    console.log('------ 服务器日志 ------');
 
-    // 在默认浏览器中打开网站(Mac下无效)
-    if (config.isAutoOpenDefaultPage) {
-        var cp = require('child_process');
-        cp.exec('start http://localhost:' + port + config.defaultPage);
-    }
+// 端口占用检测,如果端口占用加一再试
+portModel.getAvailablePort(port, function (port) {
+    // 启动web服务监听
+    http.listen(port, function () {
+        // 监听失败(如端口冲突)不会执行此回调
+        console.log('本地服务器 http://localhost:' + port + ' 已经启动');
+        console.log('------ 服务器日志 ------');
+
+        // 在默认浏览器中打开网站(Mac下无效)
+        if (config.isAutoOpenDefaultPage) {
+            var cp = require('child_process');
+            cp.exec('start http://localhost:' + port + config.defaultPage);
+        }
+    });
 });
