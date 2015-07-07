749dbc69661d678026559f61521e8b2196a3fd78
zhaoxiaoqiang <longze_xq@163.com>
2015-07-07 08:28:57 +0800

    删除无用注释代码

--- server/lib/start-server.js
+++ server/lib/start-server.js
@@ -19,8 +19,6 @@ portModel.getAvailablePort(port, function (port) {
     http.listen(port, function () {
         // 监听失败(如端口冲突)不会执行此回调
         console.log('本地服务器 http://localhost:' + port + ' 已经启动');
-        // console.log('首页地址 http://localhost:' + port + config.defaultPage);
-
         console.log('------ 服务器日志 ------');
 
         // 在默认浏览器中打开网站
