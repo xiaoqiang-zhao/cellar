98f9ef8ac7031cd8482d66b4d0759ddb06307dba
zhaoxiaoqiang <longze_xq@163.com>
2015-08-19 19:23:26 +0800

    丰富数据demo

--- service/demo/getJson.js
+++ service/demo/getJson.js
@@ -3,7 +3,8 @@
  */
 module.exports = {
     contentType: 'json',
-    data: function (request, response, response200) {
+    data: function (request, response, response200, response500) {
+        // 直接返回
         return {
             "status": 0,
             "result": {
@@ -11,5 +12,30 @@ module.exports = {
             },
             "statusInfo": ""
         };
+
+        // 异步返回
+        /*
+        var contentType = this.contentType;
+        setTimeout(function () {
+            // 业务逻辑代码
+            // ...
+
+            // 返回异步执行后的结果
+            response200(
+                contentType,
+                {
+                    "status": 0,
+                    "result": {
+                        "description": '描述'
+                    },
+                    "statusInfo": ""
+                }
+                // ,'utf-8' 编码格式，
+            );
+
+            // 如果有错误，在可以在这里向外抛异常
+            // response500();
+        }, 0);
+        */
     }
 };
