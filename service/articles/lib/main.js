5c91f92c2fe14e0fef590a973a3ed4828490c457
zhaoxiaoqiang <longze_xq@163.com>
2015-08-25 19:32:48 +0800

    添加探索博客的创建

--- service/articles/lib/main.js
+++ service/articles/lib/main.js
@@ -4,7 +4,23 @@
  * Created by v_zhaoxiaoqiang on 2015/6/1.
  */
 module.exports = {
-    getList: function (articleName, request, response) {
+    contentType: 'json',
+    data: function (request, response, response200, response500) {
+
+        //var dataServer = require('./data-server');
+        //var ref = new dataServer('/https://longze.wilddogio.com');
+        //ref.on('value', function (datasnapshot) {
+        //    response200(
+        //        contentType,
+        //        {
+        //            "status": 0,
+        //            "result": datasnapshot.val(),
+        //            "statusInfo": ""
+        //        }
+        //        // ,'utf-8' 编码格式，
+        //    );
+        //});
+
         return '临时，访问名：' + articleName;
     }
 };
