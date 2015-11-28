92172e1362b09df5f4852f8cc9bd538542fd65ce
zhaoxiaoqiang <longze_xq@163.com>
2015-11-28 23:10:07 +0800

    添加 c++ 学习笔记内容 - 自定义类的封装和使用

--- web/articles/c++/lab/Plane.hpp
+++ web/articles/c++/lab/Plane.hpp
@@ -23,48 +23,6 @@ class Plane {
     void doSomething();
 };
 
-/**************** 实现 ****************/
-
-// 构造函数,用来初始化对象
-Plane::Plane(int speed) {
-    setSpeed(speed);
-}
-
-// 析构函数,用来释放对象内存空间
-Plane::~Plane() {
-
-}
-
-void Plane::setSpeed(int newSpeed) {
-    speed = newSpeed;
-}
-void Plane::printCurrentSpeed() {
-    cout<<"current speed:"<< speed <<"\n";
-}
-
-/**
- * 加速
- *
- * @param {int} num 要增加的速度
- * @return {int} speed 增加之后的速度
- */
-int Plane::speedUp(int num) {
-     speed += num;
-     doSomething();
-     return speed;
-}
-void Plane::speedDown(int num) {
-    speed -= num;
-    if (speed < 0) {
-        speed = 0;
-        cout<<"speed has benn zero.\n";
-    }
-    doSomething();
-}
-void Plane::doSomething() {
-    cout<<"please bee careful, speed is changing.\n";
-}
-
 /* 测试方法，可以放在任何文件中进行测试
 
 #include <iostream>
