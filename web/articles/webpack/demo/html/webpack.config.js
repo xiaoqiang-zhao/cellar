e352da61a862bec37124a60e9856645e3f291999
zhaoxiaoqiang <longze_xq@163.com>
2015-12-06 17:43:09 +0800

    webpack demo 目录修改

---
+++ web/articles/webpack/demo/html/webpack.config.js
@@ -0,0 +1,21 @@
+require('webpack/html-loader');
+module.exports = {
+    entry: {
+        main: './a.js'
+    },
+
+    output: {
+        path: './dist/',
+        publicPath: '/js/',
+        filename: '[name].js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ]
+    }
+};
