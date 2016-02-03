c48f4d45966f6fd6e1dbe7f9fb7e671a9545a482
zhaoxiaoqiang <longze_xq@163.com>
2016-02-03 13:19:42 +0800

    将 webpack 配置提取到单独模块

---
+++ tool/init-site/lib/webpack-config.js
@@ -0,0 +1,48 @@
+/**
+ * Created by zhaoxiaoqiang on 16/2/3.
+ */
+
+var libPath = __dirname + '/';
+var rootPath = libPath.replace('tool/init-site/lib/', '');
+
+var webpackConfig = {
+    entry: {
+        main: rootPath + 'web/src/components/main/main.js'
+    },
+    resolve: {
+        alias: {
+            vue: rootPath + '/web/src/dep/vue.js'
+        }
+    },
+    optimize: {
+        // 是否压缩
+        minimize: true // UglifyJsPlugin
+    },
+    output: {
+        path: rootPath + '/web/dist/',
+        filename: '[hash].js'
+    },
+    module: {
+        loaders: [
+            //{
+            //    test: /\.js$/,
+            //    loader: 'babel-loader' //?stage=1
+            //    ,exclude: /(dep)|(dist)/
+            //},
+            {
+                test: /\.tpl$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000"
+            }
+        ]
+    }
+};
+
+module.exports = webpackConfig;
